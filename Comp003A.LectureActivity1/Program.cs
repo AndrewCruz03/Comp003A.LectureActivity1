a60a75d3d24b03fa198de63d143e26cae50b13ea
AndrewCruz03 <andrewcruzpacheco@my.whccd.edu>
2025-01-15 20:51:44 -0800

    added author course, faculity, and purpose comments

--- Comp003A.LectureActivity1/Program.cs
+++ Comp003A.LectureActivity1/Program.cs
@@ -1,4 +1,12 @@
-﻿namespace Comp003A.LectureActivity1
+﻿/*
+    Author: Andrew Cruz Pacheco
+    Course Comp-3A
+    Faculty: Jonathan Cruz
+    Purpose Basic console app
+
+*/
+
+namespace Comp003A.LectureActivity1
 {
     internal class Program
     {
@@ -8,6 +16,10 @@
 
         // main level
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi! \nMy name is Andrew Cruz");
+            Console.WriteLine("Here are 10 things about me");
+            Console.WriteLine("!.Im from Hanford\n2.I have 3 younger siblings\n3.I enjoy wathhcing anime\n4.I like playing video games\n5.My major is Mechanical Engineering\n6.My favorite soccer team is FC Barcelona\n7.A hobby Im gettin into is 3d printing\n8.I enjoy Bowling, my highest score is 172\n9.I also enjoy Playing basketball\n10.Im currently attending 3 campuses"); 
+                
         }
     }
 }
