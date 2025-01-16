eaa17fefe59cdf81c19b7d2c6e50e47044ebb68f
AndrewCruz03 <andrewcruzpacheco@my.whccd.edu>
2025-01-15 19:59:00 -0800

    Add project files.

---
+++ Comp003A.LectureActivity1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Comp003A.LectureActivity1
+{
+    internal class Program
+    {
+    //class level
+        static void Main(string[] args)
+        {
+
+        // main level
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
